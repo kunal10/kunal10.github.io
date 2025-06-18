f8f1ff84d26418ec154056db6864d483d57985ef
kunal10 <kunal10@users.noreply.github.com>
2025-06-18 15:31:24 +0000

    Deploying to gh-pages from @ kunal10/kunal10.github.io@66ab1afda2fbe62e94da68be0c534f57f6812875 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-publications",
+          title: "Publications",
+          description: "My research publications in reverse chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
