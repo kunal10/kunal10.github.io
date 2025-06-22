1cc641dcef9cfdc745719dc0a03be5c0b6304c74
kunal10 <kunal10@users.noreply.github.com>
2025-06-22 18:18:21 +0000

    Deploying to gh-pages from @ kunal10/kunal10.github.io@ce0786526bc9894bc9c32ed4ee96a5c0bc22f3d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -151,6 +151,13 @@ ninja.data = [{
           window.open("https://twitter.com/kunallad423", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=3N4bVMsAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
