d5a41facdb13e135124852d8b2f5956e118c1bc7
kunal10 <kunal10@users.noreply.github.com>
2025-06-24 19:38:55 +0000

    Deploying to gh-pages from @ kunal10/kunal10.github.io@8694d013ebff0184e65b0e51638ac62a34f5b511 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -87,9 +87,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-video-summarization",
+          title: 'Video Summarization',
+          description: "Deep Learning system to generate a summary of the most vital parts of the video.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
