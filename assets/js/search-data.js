fcc6ef4af84f401ed743e1d212e51c68aa95ccb2
kunal10 <kunal10@users.noreply.github.com>
2025-06-25 15:53:42 +0000

    Deploying to gh-pages from @ kunal10/kunal10.github.io@c4f684d5a216ea82d492472e5599c005d669804a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of my cool projects.",
+          description: "A curated collection of my creative and technical projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -72,36 +72,11 @@ ninja.data = [{
           description: "Tap into knowledge from trusted OpenStax textbooks in Gemini",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-video-summarization",
           title: 'Video Summarization',
           description: "Deep Learning system to generate a summary of the most vital parts of the video.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
