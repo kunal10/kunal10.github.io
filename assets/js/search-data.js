1f6d31e7a27bc886e6af7251884d8974d3448769
kunal10 <kunal10@users.noreply.github.com>
2025-06-26 09:27:06 +0000

    Deploying to gh-pages from @ kunal10/kunal10.github.io@6c29edc1fda07e99f4f58efb4976a7961d677f96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,29 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
@@ -61,22 +39,22 @@ ninja.data = [{
           title: 'LearnLM',
           description: "AI model specifically designed and fine-tuned for educational purposes.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/LearnLM/";
             },},{id: "projects-learning-coach",
           title: 'Learning Coach',
           description: "Personalized tutoring agent to enhance the learning experience.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/LearningCoach/";
             },},{id: "projects-openstax",
           title: 'OpenStax',
           description: "Tap into knowledge from trusted OpenStax textbooks in Gemini",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/OpenStax/";
             },},{id: "projects-video-summarization",
           title: 'Video Summarization',
           description: "Deep Learning system to generate a summary of the most vital parts of the video.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/projects/VideoSummarization/";
             },},{
         id: 'social-email',
         title: 'email',
